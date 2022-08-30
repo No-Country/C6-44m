64a24a3e37cc0205b5971730616317620e47e836
felipe <felipe.calderon321@gmail.com>
2022-08-30 18:10:48 -0400

    limpiando main

--- front/components/Auth/Auth.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Login} from "../../views/auth/Login/Login";
-
-
-export default function Auth() {
-  return (
-    <Login /> 
-  )
-}
--- front/pages/contacto.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import BasicLayout from '../components/BasicLayout/BasicLayout';
-
-export default function contacto() {
-  return <BasicLayout></BasicLayout>;
-}
